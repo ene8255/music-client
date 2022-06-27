8c5ce6c62ca0a8a7731385768e6e00925361c6eb
ene8255 <86288109+ene8255@users.noreply.github.com>
2022-06-27 21:47:27 +0900

    검색 기능 보완

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiSearch } from "react-icons/fi";
 import './search.scss';
 import { Link } from 'react-router-dom';
@@ -7,23 +7,29 @@ import { API_URL } from '../../config/constants';
 import useAsync from '../../hooks/useAsync';
 
 function SearchPage() {
+    // sessionStorage에 저장된 search 값을 가져옴
+    // 페이지를 새로고침하거나 뒤로가기로 다시 돌아와도 검색값이 그대로 남아 있도록 하기 위한 목적
+    const sessionSearch = JSON.parse(window.sessionStorage.getItem("search"));
+
     // search input 값이 바뀔때마다 상태 관리
-    const [ search, setSearch ] = useState(null);
+    const [ search, setSearch ] = useState(sessionSearch || "");
 
     // 검색 결과 가져오기
     async function getResult(value) {
-        const response = await axios.get(
-            `${API_URL}/search/${value}`
-        )
-        return response.data;
+        if(search !== "") {
+            const response = await axios.get(
+                `${API_URL}/search/${value}`
+            )
+            return response.data;
+        }
     }
 
     // input의 값이 바뀔때마다 상태 업데이트
     function onChange(e) {
         const keyword = e.target.value;
         setSearch(keyword);
-        // keyword가 없으면(input의 값을 다 지우면) 다시 null로 설정하기
-        if(!keyword) setSearch(null);
+        // keyword가 없으면(input의 값을 다 지우면) 다시 empty string으로 설정하기
+        if(!keyword) setSearch("");
     }
 
     // useAsync로 search 값이 바뀔때마다 데이터 가져오기
@@ -34,12 +40,17 @@ function SearchPage() {
     function onSubmit(e) {
         e.preventDefault();
     }
-    
+
+    // search 값이 바뀔 때마다 sessionStorage의 값도 변경하기
+    useEffect(() => {
+        window.sessionStorage.setItem("search", JSON.stringify(search));
+    }, [search]);
+
     return (
         <main>
             <section id="inputSection">
                 <form onSubmit={onSubmit}>
-                    <input type="search" placeholder="음악, 아티스트, 앨범" onChange={onChange} />
+                    <input type="search" placeholder="음악, 아티스트, 앨범" value={search} onChange={onChange} />
                     <button type="submit"><FiSearch /></button>
                 </form>
             </section>
