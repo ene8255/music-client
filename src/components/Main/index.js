7f00fb7c504517fa50ff69ef9d78ac3dbeaa5835
ene8255 <86288109+ene8255@users.noreply.github.com>
2022-07-04 20:47:38 +0900

    모바일 화면 테스트

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './main.scss';
 import MainList from './MainList';
 import MainSkeleton from '../Skeletons/MainSkeleton';
@@ -15,6 +15,25 @@ async function getPlaylists() {
 }
 
 function MainPage() {
+    // 모바일 화면일때 touchX 값 관리
+    const [ touchX, setTouchX ] = useState(0);
+
+    // 화면 터치가 시작되면 X값을 설정함
+    function onTouchStart(e) {
+        setTouchX(e.changedTouches[0].pageX);
+    }
+
+    // 화면 터치가 완료되면 해당 ul의 left값만 변경함
+    function onTouchEnd(e) {
+        const distanceX = touchX - e.changedTouches[0].pageX;
+        const targetUl = e.target.closest('ul');
+        const newLeft = Math.abs(parseFloat(targetUl.style.left)) + distanceX;
+
+        if(newLeft < 450) {
+            targetUl.style.left = `-${newLeft}px`;
+        }
+    }
+
     // useAsync로 data 받아오는 상태 관리
     const state = useAsync(getPlaylists);
     const { loading, error, data: playlists } = state;
@@ -32,13 +51,18 @@ function MainPage() {
             {filteredGroups.map(group => (
                 <section className='mainSection' key={group}>
                     <h2>{group}에 맞는 플레이리스트</h2>
-                    <ul>
-                        {playlists.map(item => 
-                            group === item.p_group ? 
-                            <MainList key={item.p_id} data={item} /> : 
-                            null
-                        )}
-                    </ul>
+                    <div id='mobilePDiv'>
+                        <ul style={{ left: '0px' }} 
+                            onTouchStart={onTouchStart} 
+                            onTouchEnd={onTouchEnd}
+                        >
+                            {playlists.map(item => 
+                                group === item.p_group ? 
+                                <MainList key={item.p_id} data={item} /> : 
+                                null
+                            )}
+                        </ul>
+                    </div>
                 </section>
             ))}
         </main>
